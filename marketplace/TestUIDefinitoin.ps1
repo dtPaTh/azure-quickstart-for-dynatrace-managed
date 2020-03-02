df52854ffa8054f873df1f55b4c7886e6dd21e94
Patrick Thurner <patrick.thurner@gmail.com>
2020-03-02 18:40:58 +0100

    Add testing scripts

---
+++ marketplace/TestUIDefinitoin.ps1
@@ -0,0 +1,5 @@
+﻿Connect-AzureRmAccount
+
+Select-AzureRMSubscription -Subscription "DemoEnvironment-Dev"
+
+.\SideLoad-CreateUIDefinition.ps1 -createUIDefFile ".\createUIDefinition.json"
---
+++ marketplace/ValidateTemplates.ps1
@@ -0,0 +1,16 @@
+﻿
+##Import-Module ..\..\arm-ttk\arm-ttk\arm-ttk.psd1
+
+Remove-Item -Path ".\test-temp" -Recurse
+New-Item -Name "test-temp" -ItemType "directory"
+Copy-Item "createUiDefinition.json" -Destination ".\test-temp\"
+Copy-Item "..\mainTemplate.json" -Destination ".\test-temp"
+
+& "..\..\arm-ttk\arm-ttk\test-AzTemplate.cmd"
+#$TestResults = Test-AzTemplate -TemplatePath ".\test-temp" 
+#$TestFailures =  $TestResults | Where-Object { -not $_.Passed }
+#$FailureTargetObjects = $TestFailures | Select-Object -ExpandProperty Errors |  Select-Object -ExpandProperty TargetObject
+
+#Write-Output $FailureTargetObjects
+
+#Remove-Item -Path ".\test-temp" -Recurse
