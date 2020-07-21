7f9133fd273f2c93c0ffef715ca1bf6dced9b462
Patrick Thurner <patrick.thurner@gmail.com>
2020-07-21 09:13:03 +0200

    Adapt latest installer enhancements

--- custom-scripts/installmanaged.sh
+++ custom-scripts/installmanaged.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-
 managedLicenseKey="$1"
 initialEnvironmentName="$2"
 initialEnvironmentAdminFirstname="$3"
 initialEnvironmentAdminLastname="$4"
 initialEnvironmentAdminEmail="$5"
 initialEnvironmentAdminSecret="$6"
-nodeId="$7"
-fqdn="$8"
-installerDownloadUrl="$9"
+installerDownloadUrl="$7"
+
 
 LOGFILE='/tmp/install-managed-extension.log'
-log() {
-    echo $1 >> $LOGFILE
-}
 
+log() {
+    echo "[CustomScript] $1" >> $LOGFILE
+}
 
 log "run installmanaged.sh"
 
@@ -23,47 +21,38 @@ log 'prepare datadisks'
 
 sudo bash prepare_vm_disks.sh >> $LOGFILE
 
-log 'download latest installer'
-wget "$installerDownloadUrl" -O /tmp/dt-mgd-install.sh 1>> $LOGFILE
+log 'download latest installer...'
+if curl --fail --silent --show-error "$installerDownloadUrl" --output /tmp/dt-mgd-install.sh ; then
+    log 'done.'
+else
+    log 'failed.'
+    exit -1
+fi
+
 
 log 'execute installer'
 sudo sh /tmp/dt-mgd-install.sh --timeouts fw:120 --install-silent --license "$managedLicenseKey" --datastore-dir /datadisks/disk1/dynatrace --svr-datastore-dir /datadisks/disk2/dynatrace --cas-datastore-dir /datadisks/disk3/dynatrace --els-datastore-dir /datadisks/disk4/dynatrace --initial-environment "$initialEnvironmentName" --initial-first-name "$initialEnvironmentAdminFirstname" --initial-last-name "$initialEnvironmentAdminLastname" --initial-email "$initialEnvironmentAdminEmail" --initial-pass "$initialEnvironmentAdminSecret"  1>> $LOGFILE
 
-publicIp=""
-if [ -z "$fqdn" ] 
-then
-    log "FQDN not provided"
-else
-    publicIp=$(host "$fqdn"|grep " has address "|cut -d" " -f4)
-
-    log "Resolved ip ($publicIp) from FQDN($fqdn)"
-fi
+publicIp=$(curl -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/publicIpAddress?api-version=2019-06-01&format=text")
 
 if [ -z "$publicIp" ] 
 then
     log "[WARNING] Couldn't resolve public IP, skip config to define endpoint for webui"
 else
-    for i in {1..3}
-    do
-        log 'try set nodes public ip'
-        if curl --fail -X PUT --insecure -u "admin":"$initialEnvironmentAdminSecret" "https://127.0.0.1:8021/api/v1.0/onpremise/endpoint/publicIp/domain/$nodeId" -d "$publicIp" -H "Content-Type: application/json" 1>> $LOGFILE; then
-            break
-        else
-            waitTime=$((i*3))
-            log "[WARNING] unable to set public ip, retry in $waitTime minute(s) to ensure server is responsive..."
-            sleep "$waitTime""m"
-        fi
-    done
+    log 'try set nodes public ip'
+    curl --fail --silent -X PUT --insecure -u "admin":"$initialEnvironmentAdminSecret" "https://127.0.0.1:8021/api/v1.0/onpremise/endpoint/publicIp/domain/1" -d "$publicIp" -H "Content-Type: application/json" 1>> $LOGFILE;
 fi
 
 log 'get api-token'
-token=$(wget --http-user=admin --http-password="$initialEnvironmentAdminSecret"  -q https://127.0.0.1:8021/api/v1.0/onpremise/tokens/ --no-check-certificate -O - | grep -o 'tokenId":".[^"]*' | cut -b11-)
+token=$(wget --http-user=admin --http-password="$initialEnvironmentAdminSecret"  -q https://127.0.0.1:8021/api/v1.0/onpremise/tokens/ --no-check-certificate -O - | grep -o 'tokenId":".[^"]*' | cut -b11- | head -n1)
 
 if [ -z "$token" ] 
 then
     log '[ERROR] could not retrieve api-token'
+    echo 'Could not rereive api-token'
+   
     exit -1
 else
-    echo "$token"
     log 'finished successfully'
+    echo "$token" #write token to std output, so it can be passed over to node installers
 fi
--- custom-scripts/installmanagednode.sh
+++ custom-scripts/installmanagednode.sh
@@ -5,12 +5,12 @@ seedIp="$2"
 seedAuthToken="$3"
 initialEnvironmentAdminSecret="$4"
 nodeId="$5"
-fqdn="$6"
-installerDownloadUrl="$7"
+installerDownloadUrl="$6"
 
 LOGFILE='/tmp/install-managed-extension.log'
+
 log() {
-    echo $1 >> $LOGFILE
+    echo "[CustomScript] $1" >> $LOGFILE
 }
 
 log "run installmanagednode.sh"
@@ -19,7 +19,12 @@ log 'prepare datadisks'
 sudo bash prepare_vm_disks.sh 1>> $LOGFILE
 
 log 'download latest installer'
-wget "$installerDownloadUrl" -O /tmp/dt-mgd-install.sh 1>> $LOGFILE
+if curl --fail --silent --show-error "$installerDownloadUrl" --output /tmp/dt-mgd-install.sh; then 
+    log 'done.'
+else
+    log 'failed.'
+    exit -1
+fi
 
 log 'execute installer'
  
@@ -31,34 +36,14 @@ log 'execute installer'
  #--rpl-datastore-dir <path> full path to Dynatrace session replay store
 sudo sh /tmp/dt-mgd-install.sh --timeouts fw:120 --install-silent --license "$managedLicenseKey" --seed-ip "$seedIp" --seed-auth "$seedAuthToken" --datastore-dir /datadisks/disk1/dynatrace --svr-datastore-dir /datadisks/disk2/dynatrace --cas-datastore-dir /datadisks/disk3/dynatrace --els-datastore-dir /datadisks/disk4/dynatrace  1>> $LOGFILE
 
-publicIp=""
-if [ -z "$fqdn" ] 
-then
-    log "FQDN not provided"
-else
-    publicIp=$(host "$fqdn"|grep " has address "|cut -d" " -f4)
-
-    log "Resolved ip ($publicIp) from FQDN($fqdn)"
-fi
+publicIp=$(curl -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/publicIpAddress?api-version=2019-06-01&format=text")
 
 if [ -z "$publicIp" ] 
 then
-    log "[WARNING] Couldn't resolve public IP, skip config to define endpoint for webui"
+    log "[WARNING] Couldn't resolve public Ip, skip config to define endpoint for webui"
 else
-    
-
-    for i in {1..3}
-    do
-        log 'try set nodes public ip'
-        if curl --fail -X PUT --insecure -u "admin":"$initialEnvironmentAdminSecret" "https://127.0.0.1:8021/api/v1.0/onpremise/endpoint/publicIp/domain/$nodeId" -d "$publicIp" -H "Content-Type: application/json" 1>> $LOGFILE; then
-            break
-        else
-            waitTime=$((i*3))
-            log "[WARNING] unable to set public ip, retry in $waitTime minute(s) to ensure server is responsive..."
-            sleep "$waitTime""m"
-        fi
-    done
-  
+    log 'try set nodes public ip'
+    curl --fail --silent -X PUT --insecure -u "admin":"$initialEnvironmentAdminSecret" "https://127.0.0.1:8021/api/v1.0/onpremise/endpoint/publicIp/domain/$nodeId" -d "$publicIp" -H "Content-Type: application/json" 1>> $LOGFILE
 fi
 
 log 'finished successful'
